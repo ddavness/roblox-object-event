5c9ddea990270085851925b997b72e8c3895b63c
David Duque <david.f.s.duque@tecnico.ulisboa.pt>
2019-07-20 18:29:53 +0100

    Fixing runtime tabulation
    - Preparing to pack and upload to NPM.
    
    - Somehow VSCode messed up tabulation on the .lua file. Fixed it.
    
    - Since the package (seemingly) works, the beta tag was removed from the package description.
    
    Signed-off-by: David Duque <david.f.s.duque@tecnico.ulisboa.pt>

--- init.lua
+++ init.lua
@@ -23,91 +23,95 @@ local ObjectEvent, ObjectEventConnection
 
 ObjectEvent = setmetatable({}, {
     __tostring = function() return "ObjectEvent"; end
-}); do
+})
+
+do
     ObjectEvent.__index = ObjectEvent
-    
-	function ObjectEvent.new(...)
-		local self = setmetatable({}, ObjectEvent)
+
+    function ObjectEvent.new(...)
+        local self = setmetatable({}, ObjectEvent)
         self.SubscribedConnections = {}
         self.YieldQueue = {}
 
         return self
     end;
-    
-	function ObjectEvent:Connect(f)
-		return EventConnection.new(f, self);
+
+    function ObjectEvent:Connect(f)
+        return EventConnection.new(f, self)
     end
-    
-	function ObjectEvent:Wait()
-		table.insert(self.YieldQueue, coroutine.running())
-		return coroutine.yield();
+
+    function ObjectEvent:Wait()
+        table.insert(self.YieldQueue, coroutine.running())
+        return coroutine.yield()
     end
-    
-	function ObjectEvent:Fire(...)
+
+    function ObjectEvent:Fire(...)
         local args = {...}
-        
+
         -- Activate all listeners
-		for _, c in pairs(self.SubscribedConnections) do
-			coroutine.resume(coroutine.create(function()
-				connection.Listener(args)
-			end))
+        for _, c in pairs(self.SubscribedConnections) do
+            coroutine.resume(coroutine.create(function()
+                connection.Listener(args)
+            end))
         end
-        
+
         -- Resume all yielding threads on :Wait()
-		for _, t in pairs(self.YieldQueue) do
-			coroutine.resume(thread, args);
+        for _, t in pairs(self.YieldQueue) do
+            coroutine.resume(thread, args)
         end
-        
+
         -- Reset the yielding queue.
-		self.YieldQueue = {}
+        self.YieldQueue = {}
     end 
 end
 
 ObjectEventConnection = setmetatable({}, {
-    __tostring = function() return "EventConnection"; end
-}); do
+__tostring = function() return "EventConnection"; end
+})
+
+do
     ObjectEventConnection.__index = ObjectEventConnection
-    
-	function ObjectEventConnection.new(toConnect, event)
-		local self = setmetatable({}, ObjectEventConnection)
+
+    function ObjectEventConnection.new(toConnect, event)
+        local self = setmetatable({}, ObjectEventConnection)
         self.Listener = toConnect
         self.Event = event
         self.Enabled = false
         -- Forcing reconnect should do the trick
         self:Reconnect()
 
-		return self
-	end;
-
-	function ObjectEventConnection:IsConnected()
-		return self.Enabled;
-    end;
-    
-	function ObjectEventConnection:Disconnect()
-		local subscribers = self.Event.SubscribedConnections
-        local index = indexFromValue(subscribers, self)
-        
-		if not index then
-			warn("Attempted to disconnect an event that wasn't connected at all. Exiting.", 0)
-			return nil
-        end
-        
-		table.remove(subscribers, index);
-		self.Enabled = false;
+        return self
     end
-    
-	function ObjectEventConnection:Reconnect()
-		local subscribers = self.Event.SubscribedConnections
+
+    function ObjectEventConnection:IsConnected()
+        return self.Enabled;
+    end
+
+    function ObjectEventConnection:Disconnect()
+        local subscribers = self.Event.SubscribedConnections
         local index = indexFromValue(subscribers, self)
-        
-		if index then
-			warn("Attempted to reconnect an event that is already connected. Exiting.", 0)
-			return nil
+
+        if not index then
+            warn("Attempted to disconnect an event that wasn't connected at all. Exiting.", 0)
+            return nil
         end
-        
-		table.insert(subscribers, self);
-		self.Enabled = true;
-	end
+
+        table.remove(subscribers, index)
+        self.Enabled = false
+    end
+
+    function ObjectEventConnection:Reconnect()
+        local subscribers = self.Event.SubscribedConnections
+        local index = indexFromValue(subscribers, self)
+
+        if index then
+            warn("Attempted to reconnect an event that is already connected. Exiting.", 0)
+            return nil
+        end
+
+        table.insert(subscribers, self);
+        self.Enabled = true;
+    end
 end
 
 return {ObjectEvent}
