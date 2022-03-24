81fa4932f9706cbbd0eb44fa5be41cfa3b1829b6
David Duque <david@duqued.net>
2022-03-24 01:53:00 +0000

    Add BindableEvent-like interface

--- init.lua
+++ init.lua
@@ -8,98 +8,122 @@
 --]]
 
 -- Declare existance of our classes
-local ObjectEvent, ObjectEventConnection
+local RBXScriptSignalLike, ObjectEvent, ObjectEventConnection
+
+RBXScriptSignalLike = setmetatable({}, {
+	__tostring = function()
+		return "RBXScriptSignalLike"
+	end
+})
+
+RBXScriptSignalLike.__index = RBXScriptSignalLike
+
+function RBXScriptSignalLike.new()
+	local self = setmetatable({}, RBXScriptSignalLike)
+	self.SubscribedConnections = {}
+	self.YieldQueue = {}
+
+	return self
+end
+
+function RBXScriptSignalLike:Connect(f)
+	return ObjectEventConnection.new(f, self)
+end
+
+function RBXScriptSignalLike:Wait()
+	table.insert(self.YieldQueue, coroutine.running())
+	return coroutine.yield()
+end
 
 ObjectEvent = setmetatable({}, {
-    __tostring = function()
-        return "ObjectEvent"
-    end
+	__tostring = function()
+		return "ObjectEvent"
+	end
 })
 
 ObjectEvent.__index = ObjectEvent
 
 function ObjectEvent.new()
-    local self = setmetatable({}, ObjectEvent)
-    self.SubscribedConnections = {}
-    self.YieldQueue = {}
+	local self = setmetatable({}, ObjectEvent)
+	self.Event = RBXScriptSignalLike.new()
 
-    return self
+	return self
 end
 
 function ObjectEvent:Connect(f)
-    return ObjectEventConnection.new(f, self)
+	return self.Event.Connect(self.Event, f)
 end
 
 function ObjectEvent:Wait()
-    table.insert(self.YieldQueue, coroutine.running())
-    return coroutine.yield()
+	return self.Event.Wait(self.Event)
 end
 
 function ObjectEvent:Fire(...)
-    local args = {...}
+	local args = {...}
 
-    -- Activate all listeners
-    for _, c in pairs(self.SubscribedConnections) do
-        task.spawn(function()
-            c.Listener(unpack(args))
-        end)
-    end
+	-- Activate all listeners
+	for _, c in pairs(self.Event.SubscribedConnections) do
+		task.spawn(function()
+			c.Listener(unpack(args))
+		end)
+	end
 
-    -- Resume all yielding threads on :Wait()
-    for _, t in pairs(self.YieldQueue) do
-        task.spawn(t, unpack(args))
-    end
+	-- Resume all yielding threads on :Wait()
+	for _, t in pairs(self.Event.YieldQueue) do
+		task.spawn(t, unpack(args))
+	end
 
-    -- Reset the yielding queue.
-    self.YieldQueue = {}
+	-- Reset the yielding queue.
+	self.YieldQueue = {}
 end
 
 ObjectEventConnection = setmetatable({}, {
-__tostring = function() return "EventConnection"; end
+	__tostring = function()
+		return "EventConnection"
+	end
 })
-
 
 ObjectEventConnection.__index = ObjectEventConnection
 
 function ObjectEventConnection.new(toConnect, event)
-    local self = setmetatable({}, ObjectEventConnection)
-    self.Listener = toConnect
-    self.Event = event
-    self.Enabled = false
-    -- Forcing reconnect should do the trick
-    self:Reconnect()
+	local self = setmetatable({}, ObjectEventConnection)
+	self.Listener = toConnect
+	self.Event = event
+	self.Enabled = false
+	-- Forcing reconnect should do the trick
+	self:Reconnect()
 
-    return self
+	return self
 end
 
 function ObjectEventConnection:IsConnected()
-    return self.Enabled;
+	return self.Enabled;
 end
 
 function ObjectEventConnection:Disconnect()
-    local subscribers = self.Event.SubscribedConnections
-    local index = table.find(subscribers, self)
+	local subscribers = self.Event.SubscribedConnections
+	local index = table.find(subscribers, self)
 
-    if not index then
-        warn("Attempted to disconnect an event that wasn't connected at all. Exiting.", 0)
-        return nil
-    end
+	if not index then
+		warn("Attempted to disconnect an event that wasn't connected at all. Exiting.", 0)
+		return nil
+	end
 
-    table.remove(subscribers, index)
-    self.Enabled = false
+	table.remove(subscribers, index)
+	self.Enabled = false
 end
 
 function ObjectEventConnection:Reconnect()
-    local subscribers = self.Event.SubscribedConnections
-    local index = table.find(subscribers, self)
+	local subscribers = self.Event.SubscribedConnections
+	local index = table.find(subscribers, self)
 
-    if index then
-        warn("Attempted to reconnect an event that is already connected. Exiting.", 0)
-        return nil
-    end
+	if index then
+		warn("Attempted to reconnect an event that is already connected. Exiting.", 0)
+		return nil
+	end
 
-    table.insert(subscribers, self);
-    self.Enabled = true;
+	table.insert(subscribers, self);
+	self.Enabled = true;
 end
 
 return ObjectEvent
